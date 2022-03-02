1ab4afd1a0c1109f6821c5fe21ddc4e788b4f9f6
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-03 00:49:11 +0330

    dashboard

--- Sources/04. Endpoints/PhoneBook.Endpoints.UI.MVC/Controllers/HomeController.cs
+++ Sources/04. Endpoints/PhoneBook.Endpoints.UI.MVC/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Endpoints.UI.MVC.Models.AAA;
+using PhoneBook.Infrastructures.DAL.EF.Phones;
 
 namespace PhoneBook.Endpoints.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserDbContext userDbContext;
+
+        public HomeController(UserDbContext userDbContext)
+        {
+            this.userDbContext = userDbContext;
+        }
+
         public IActionResult Index()
         {
             ViewBag.PageTitle = "Admin Panel";
+
+            ViewBag.UserCount = userDbContext.Users.Count();
+            ViewBag.PhoneCount = 12;
+            //ViewBag.PhoneCount = phoneRepository.GetAll().Count();
+
 
             return View();
         }
